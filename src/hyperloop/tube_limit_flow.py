94f96012e854ee3a5132c8f589fbbceec742265f
Brent Schroeter <brentsch@uw.edu>
2015-09-26 12:41:08 -0700

    Incomplete updates

--- src/hyperloop/tube_limit_flow.py
+++ src/hyperloop/tube_limit_flow.py
@@ -7,54 +7,114 @@ import pylab
 from openmdao.core.component import Component
 from openmdao.units.units import convert_units as cu
 
-from pycycle import flowstation
+from pycycle.set_total import SetTotal
+from pycycle.thermo_static import SetStaticMN, SetStaticPs
+from pycycle.constants import AIR_MIX
 
-class TubeLimitFlow(Component):
+class AreaRatio(Component):
+    def __init__(self):
+        super(AreaRatio, self).__init__()
+        self.add_param('tube_r', 0.0, desc='inner radius of tube', units='m')
+        self.add_param('inlet_area', 0.0, desc='area of inlet at largest point', units='m**2')
+        self.add_param('gamma', 1.41, desc='ratio of specific heats')
+        self.add_param('Mach', 1.0, desc='travel Mach')
+        self.add_param('Mach_bypass', 0.95, desc='Mach of air passing around pod') # used in TubeLimitFlow
+
+        self.add_output('bypass_area', 0.0, desc='area between tube wall and pod', units='m**2')
+        self.add_output('AR', 0.0, desc='ratio between tube area and bypass area')
+
+        self.add_state('AR_resid', 0.0, desc='AR - target AR')
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        self.apply_nonlinear(params, unknowns, resids)
+
+    def apply_nonlinear(self, params, unknowns, resids):
+        tube_area = pi * (params['tube_r'] ** 2)
+        unknowns['bypass_area'] = tube_area - params['inlet_area']
+        AR_target = tube_area / unknowns['bypass_area']
+        g = params['gamma']
+        g_exp = (g + 1.0) / (2.0 * (g - 1.0))
+        unknowns['AR'] = ((g + 1.0) / 2.0) ** (-1.0 * g_exp) * ((1.0 + (g - 1.0) / 2.0 * params['Mach'] ** 2) ** g_exp) / params['Mach']
+        resids['AR_resid'] = unknowns['AR'] - AR_target
+
+class TubeThermo(Component):
+    def __init__(self):
+        super(TubeThermo, self).__init__()
+        self.add_param('Ps', 99.0, desc='static pressure in tube', units='Pa')
+        self.add_param('Ts', 292.1, desc='static temperature in tube', units='degK')
+        self.add_param('Mach', 1.0, desc='travel Mach')
+        self.add_param('gamma', 1.41, desc='ratio of specific heats')
+
+        self.add_output('Pt', 0.0, desc='total pressure in tube', units='Pa')
+        self.add_output('Tt', 0.0, desc='total temperature in tube', units='Tt')
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        multiplier = (1.0 + (params['gamma'] - 1.0) / 2.0 * params['Mach'] ** 2)
+        unknowns['Pt'] = params['Ps'] * multiplier ** (params['gamma'] / (params['gamma'] - 1.0))
+        unknowns['Tt'] = params['Ts'] * multiplier
+
+class TubeAero(Component):
+    def __init__(self):
+        super(TubeAero, self).__init__()
+        self.add_param('velocity_tube', 0.0, desc='travel speed where choking occurs', units='m/s')
+        self.add_param('velocity_bypass', 0.0, desc='bypass speed where choking occurs', units='m/s')
+        self.add_param('bypass_area', 0.0, desc='bypass area', units='m**2')
+        self.add_param('tube_r', 0.0, desc='inner radius of tube', units='m')
+        self.add_param('rho_tube', 0.0, desc='density in tube', units='g/cm**3')
+        self.add_param('rho_bypass', 0.0, desc='density in bypass', units='g/cm**3')
+
+        self.add_output('tube_area', 0.0, desc='cross sectional area of tube', units='m**2')
+        self.add_output('W_tube', 0.0, desc='tube demand flow', units='kg/s')
+        self.add_output('W_kant', 0.0, desc='Kantrowitz limit flow', units='kg/s')
+        self.add_output('W_excess', 0.0, desc='Excess mass flow above the Kantrowitz limit', units='kg/s')
+
+    def solve_nonlinear(self, params, unknowns, resids):
+        unknowns['tube_area'] = pi * params['tube_r'] ** 2
+        unknowns['W_tube'] = params['rho_tube'] * params['velocity_tube'] * pi * params['tube_r'] ** 2
+        unknowns['W_kant'] = params['rho_bypass'] * params['velocity_bypass'] * params['bypass_area']
+        unknowns['W_excess'] = unknowns['W_tube'] - unknowns['W_kant']
+
+class TubeLimitFlow(Group):
     '''Finds the limit velocity for a body traveling through a tube'''
     def __init__(self):
         super(TubeLimitFlow, self).__init__()
-        self.add_param('r_tube', 1.115, desc='required radius for tube', units='m')
-        self.add_param('r_inlet', 0.737, desc='radius of inlet at its largest point', units='m')
-        self.add_param('Ps', 99.0, desc='static pressure in tube', units='Pa')
-        self.add_param('Ts', 292.1, desc='static temperature in tube', units='degK')
-        self.add_param('Mach_pod', 1.0, desc='travel Mach of pod')
-        self.add_param('Mach_bypass', 0.95, desc='Mach of air passing around pod')
-        
-        self.add_output('limit_velocity', 0.0, desc='pod travel speed where flow choking occurs', units='m/s')
-        self.add_output('limit_Mach', 0.0, desc='pod travel mach where flow choking occurs')
-        self.add_output('W_excess', 0.0, desc='excess tube mass flow above Kantrowitz limit', units='kg/s')
-        self.add_output('W_tube', 0.0, desc='tube demand flow', units='kg/s')
-        self.add_output('W_kant', 0.0, desc='Kantrowitz limit flow', units='kg/s')
-        self.add_output('tube_area', 0.0, desc='area of tube', units='ft**2')
-        self.add_output('inlet_area', 0.0, desc='area of inlet', units='ft**2')
+        self.add('Mach_param', ParamComp('Mach', 1.0), promotes=['Mach'])
+        self.add('Mach_con', ConstraintComp('Mach > 0.0'))
+        self.add('tube_struct', TubeStructure())
+        self.add('inlet', InletGeom())
+        self.add('AR_comp', AreaRatio(), promotes=['Mach_bypass', 'bypass_area'])
+        self.add('tube_thermo', TubeThermo())
+        self.add('tube_total', SetTotal(init_reacts=AIR_MIX, mode='T'))
+        self.add('tube_static', SetStaticMN(init_reacts=AIR_MIX))
+        self.add('bypass_total', SetTotal(init_reacts=AIR_MIX, mode='T'))
+        self.add('bypass_static', SetStaticMN(init_reacts=AIR_MIX))
+        self.add('tube_aero', TubeAero(), promotes=['tube_area', 'W_tube', 'W_kant', 'W_excess'])
 
-    def solve_nonlinear(self, params, unknowns, resids):
-        r_tube = cu(params['r_tube'], 'm', 'ft')
-        r_inlet = cu(params['r_inlet'], 'm', 'ft')
-        unknowns['tube_area'] = pi * (r_tube ** 2)
-        unknowns['inlet_area'] = pi * (r_inlet ** 2)
-        bypass_area = unknowns['tube_area'] - unknowns['inlet_area']
-        Ts = cu(params['Ts'], 'degK', 'degR')
-        Ps = cu(params['Ps'], 'Pa', 'psi')
-        area_ratio_target = unknowns['tube_area'] / bypass_area
-        #iterate over pod speed until the area ratio = A_tube / A_bypass
-        out = [None] # makes out[0] a pointer
-        def f(Mach):
-            out[0] = flow_tube = flowstation.solve(Ts=Ts, Ps=Ps, Mach=Mach)
-            g = flow_tube.gamt
-            g_exp = (g + 1.0) / (2.0 * (g - 1.0))
-            AR = ((g + 1.0) / 2.0) ** (-1.0 * g_exp) * ((1.0 + (g - 1.0) / 2.0 * Mach ** 2) ** g_exp) / Mach
-            return AR - area_ratio_target
-        #Solve for Mach where AR = AR_target
-        unknowns['limit_Mach'] = newton(f, 0.3)
-        flow_tube = out[0]
-        unknowns['limit_velocity'] = cu(flow_tube.Vflow, 'ft/s', 'm/s')
-        #excess mass flow calculation
-        flow_tube = flowstation.solve(Ts=Ts, Ps=Ps, Mach=params['Mach_pod'])
-        unknowns['W_tube'] = cu(flow_tube.rhos * flow_tube.Vflow * unknowns['tube_area'], 'lbm/s', 'kg/s')
-        flow_tube = flowstation.solve(Tt=flow_tube.Tt, Pt=flow_tube.Pt, Mach=params['Mach_bypass'])
-        unknowns['W_kant'] = cu(flow_tube.rhos * flow_tube.Vflow * bypass_area, 'lbm/s', 'kg/s')
-        unknowns['W_excess'] = unknowns['W_tube'] - unknowns['W_kant']
+        self.connect('Mach', 'AR_comp.Mach')
+        self.connect('Mach', 'Mach_con.Mach')
+        self.connect('tube_struct.r_inner', 'AR_comp.tube_r')
+        self.connect('inlet.area_in', 'AR_comp.inlet_area')
+        self.connect('Mach', 'tube_thermo.Mach')
+        self.connect('tube_thermo.Pt', 'tube_total.P')
+        self.connect('tube_thermo.Tt', 'tube_total.T')
+        self.connect('tube_thermo.Pt', 'bypass_total.P')
+        self.connect('tube_thermo.Tt', 'bypass_total.T')
+        self.connect('tube_total.S', 'tube_static.S')
+        self.connect('tube_total.h', 'tube_static.ht')
+        self.connect('tube_total.P', 'tube_static.Pt')
+        self.connect('tube_total.gamma', 'tube_static.gamt')
+        self.connect('bypass_total.S', 'bypass_static.S')
+        self.connect('bypass_total.h', 'bypass_static.ht')
+        self.connect('bypass_total.P', 'bypass_static.Pt')
+        self.connect('bypass_total.gamma', 'bypass_static.gamt')
+        self.connect('Mach', 'tube_static.MN_target')
+        self.connect('Mach_bypass', 'bypass_static.MN_target')
+        self.connect('tube_static.V', 'tube_aero.velocity_tube')
+        self.connect('bypass_static.V', 'tube_aero.velocity_bypass')
+        self.connect('tube_total.rho', 'tube_aero.rho_tube')
+        self.connect('bypass_total.rho', 'tube_aero.bypass_rho')
+        self.connect('bypass_area', 'tube_aero.bypass_area')
+        self.connect('tube_struct.r_inner', 'tube_aero.tube_r')
 
 def plot_data(p, comp, c='b'):
     '''utility function to make the Kantrowitz Limit Plot''' 
@@ -62,13 +122,13 @@ def plot_data(p, comp, c='b'):
     W_tube = []
     W_kant = []
     for Mach in np.arange(.2, 1.1, .1):
-        comp.params['Mach_pod'] = Mach
+        comp.params['Mach'] = Mach
         p.run()
         Machs.append(Mach)
         W_kant.append(comp.unknowns['W_kant'])
         W_tube.append(comp.unknowns['W_tube'])
-    fig = pylab.plot(Machs, W_tube, '-', label="%3.1f Req." % (comp.unknowns['tube_area'] / comp.unknowns['inlet_area']), lw=3, c=c)
-    pylab.plot(Machs, W_kant, '--', label="%3.1f Limit" % (comp.unknowns['tube_area'] / comp.unknowns['inlet_area']), lw=3, c=c)
+    fig = pylab.plot(Machs, W_tube, '-', label="%3.1f Req." % (comp.unknowns['tube_area'] / comp.unknowns['inlet.area_in']), lw=3, c=c)
+    pylab.plot(Machs, W_kant, '--', label="%3.1f Limit" % (comp.unknowns['tube_area'] / comp.unknowns['inlet.area_in']), lw=3, c=c)
     pylab.tick_params(axis='both', which='major', labelsize=15)
     pylab.xlabel('Pod Mach Number', fontsize=18)
     pylab.ylabel('Flow Rate (kg/sec)', fontsize=18)
@@ -82,13 +142,13 @@ if __name__ == '__main__':
     comp = p.root.add('comp', TubeLimitFlow())
     p.setup()
 
-    comp.params['r_tube'] = 100.0
+    comp.params['tube_struct.r_inner'] = 100.0
     plot_data(p, comp, c='b')
 
-    comp.params['r_tube'] = 150.0
+    comp.params['tube_struct.r_inner'] = 150.0
     plot_data(p, comp, c='g')
 
-    comp.params['r_tube'] = 200.0
+    comp.params['tube_struct.r_inner'] = 200.0
     plot_data(p, comp, c='r')
 
     pylab.legend(loc='best')
