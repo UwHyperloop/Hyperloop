0c4162e4514ebbd6ab94b0e30d2a4e6a68c2c02d
Brent Schroeter <brentsch@uw.edu>
2015-10-22 16:28:06 -0700

    Streamline and fixes

--- src/hyperloop/cycle/splitter.py
+++ src/hyperloop/cycle/splitter.py
@@ -25,31 +25,23 @@ class SplitterWCalc(Component):
 
         self.mode = mode
 
-        self.add_param('W1', 0.44, desc='Weight flow for Fl_O1', units='kg/s')
+        self.add_param('W_in', 1.0, desc='total weight flow in', units='kg/s')
+        self.add_param('W1', 0.44, desc='weight flow for Fl_O1', units='kg/s')
         if mode == 'MN':
-            self.add_param('MN_out1_target', 1.0, desc='Exit Mach for Fl_O1', units='m**2')
-            self.add_param('MN_out2_target', 1.0, desc='Exit Mach for Fl_O2', units='m**2')
+            self.add_param('MN_out1_target', 1.0, desc='exit Mach for Fl_O1', units='m**2')
+            self.add_param('MN_out2_target', 1.0, desc='exit Mach for Fl_O2', units='m**2')
+
+        self.add_output('W2', 0.56, desc='Weight flow for Fl_O1', units='kg/s')
 
     def solve_nonlinear(self, params, unknowns, resids):
-        self.apply_nonlinear(self, params, unknowns, resids)
-
-    def apply_nonlinear(self, params, unknowns, resids, metadata):
-        if self.mode == 'MN':
-            pass
+        unknowns['W2'] = params['W_in'] - params['W1']
 
 
 class SplitterW(Group):
     """A Group that models a splitter with known weight flows"""
 
-    def __init__(self, thermo_data=species_data.janaf, elements=AIR_MIX, mode="MN"):
+    def __init__(self, thermo_data=species_data.janaf, elements=AIR_MIX):
         super(SplitterW, self).__init__()
-
-        if mode == "MN":
-            pass
-        else:
-            raise ValueError('Only "MN" is an allowed value for mode')
-
-        self.mode = mode
 
         self.thermo_data = thermo_data
         self.elements = elements
@@ -62,17 +54,28 @@ class SplitterW(Group):
         flow_in = FlowIn('Fl_I', self.num_prod)
         self.add('flow_in', flow_in, promotes=flow_in.flow_in_vars)
 
+        self.add('split_calc', SplitterWCalc(), promotes=['MN_out1_target', 'MN_out2_target', 'W1'])
+
+        self.connect('MN_out1_target', 'out1_stat.MN_target')
+        self.connect('MN_out2_target', 'out2_stat.MN_target')
+
+        self.connect('Fl_I:stat:W', 'split_calc.W_in')
+
         out1_stat = SetStaticMN(thermo_data, elements, 'Fl_O1:stat')
         self.add('out1_stat', out1_stat, promotes=out1_stat.flow_out_vars)
 
-        self.connect('Fl_I:tot:P', 'out1_stat.Pt')
+        self.connect('W1', 'out1_stat.W')
+        self.connect('Fl_I:tot:h', 'out1_stat.ht')
         self.connect('Fl_I:tot:S', 'out1_stat.S')
+        self.connect('Fl_I:tot:n', 'out1_stat.n_guess')
 
         out2_stat = SetStaticMN(thermo_data, elements, 'Fl_O2:stat')
         self.add('out2_stat', out2_stat, promotes=out2_stat.flow_out_vars)
 
-        self.connect('Fl_I:tot:P', 'out2_stat.Pt')
+        self.connect('split_calc.W2', 'out2_stat.W')
+        self.connect('Fl_I:tot:h', 'out2_stat.ht')
         self.connect('Fl_I:tot:S', 'out2_stat.S')
+        self.connect('Fl_I:tot:n', 'out2_stat.n_guess')
 
         # total vars
         for v_name in ('h','T','P','S','rho','gamma','Cp','Cv','n_moles'):
@@ -80,12 +83,6 @@ class SplitterW(Group):
                 self.add('%s_passthru%d' % (v_name, n), PassThrough('Fl_I:tot:%s' % v_name, 'Fl_O%d:tot:%s' % (n, v_name), 0.0), promotes=['*'])
         for n in (1, 2):
             self.add('n_passthru%d' % n, PassThrough('Fl_I:tot:n', 'Fl_O%d:tot:n' % n, np.zeros(self.num_prod)), promotes=['*'])
-
-        self.add('split_calc', SplitterWCalc(mode), promotes=['*'])
-
-        if mode == 'MN':
-            self.connect('MN_out1_target', 'out1_stat.MN_target')
-            self.connect('MN_out2_target', 'out2_stat.MN_target')
 
         for n in (1, 2):
             self.add('FAR_passthru%d' % n, PassThrough('Fl_I:FAR', 'Fl_O%d:FAR' % n, 0.0), promotes=['*'])
