e877f4d1b21f2a993e4d0551fea8c7bea54c7c65
Brent Schroeter <brentsch@uw.edu>
2015-11-08 01:00:30 -0800

    Readability and documentation

--- src/hyperloop/chart_generator.py
+++ src/hyperloop/chart_generator.py
@@ -6,12 +6,30 @@ from time import time
 import sys
 from os import devnull
 
-def plot(p, x_array, x_varname, y_varnames, x_label, y_label, title='HyperloopSim', postprocess_funcs=tuple()):
-    #if len(postprocess_funcs) == 0:
-    #    postprocess_funcs = tuple(None for var_name in y_varnames)
+def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
+        title='HyperloopSim', postprocess_funcs=tuple()):
+    '''Runs an OpenMDAO problem for multiple values of x and plots the specified
+    results.
+    
+    Parameters
+    ----------
+    p : openmdao.core.problem.problem
+    x_array : numpy.array
+        X values to sample.
+    x_varname : str
+        OpenMDAO variable name that can be set using p[`x_varname`].
+    y_varnames : list of str
+        OpenMDAO variable names that can be read using p[`y_varnames`[i]].
+    x_label, y_label : str
+        Axis labels for graph.
+    title : str
+        Title of graph.
+    postprocess_funcs : tuple of functions
+        For each y variable, if the corresponding function in the tuple exists
+        and is not equal to None, the function will be called with the y value
+        as the only parameter. The returned value will be stored and plotted.
+    '''
     progress_width = 50
-    #p.driver.options['disp'] = False
-    #p.driver.options['tol'] = 1e-8
     y_arrays = list([] for varname in y_varnames)
     opt_num = 0
     print 'Running optimizations...'
@@ -21,7 +39,9 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label, title='HyperloopSi
         elapsed_s = time() - start_time
         progress = float(opt_num) / len(x_array)
         remaining = elapsed_s / progress - elapsed_s if progress > 0 else float('nan')
-        sys.stdout.write('[%s] %.2f minutes remaining    \r' % ('#' * int(progress * progress_width) + '-' * (progress_width - int(progress * progress_width)), float(remaining) / 60))
+        sys.stdout.write('[%s] %.2f minutes remaining    \r' %
+            ('#' * int(progress * progress_width) + '-' * (progress_width - int(progress * progress_width)),
+            float(remaining) / 60))
         sys.stdout.flush()
         p[x_varname] = val
         sys.stdout = open(devnull, 'w')
@@ -34,13 +54,15 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label, title='HyperloopSi
                 out = postprocess_funcs[i](out)
             y_arrays[i].append(out)
         opt_num += 1
-    sys.stdout.write('[%s] %.2f minutes elapsed    \r' % ('#' * progress_width, float(elapsed_s) / 60))
+    sys.stdout.write('[%s] %.2f minutes elapsed    \r' %
+        ('#' * progress_width, float(elapsed_s) / 60))
     sys.stdout.flush()
     print ''
     print ''
     colors = ('r', 'g', 'b', 'c', 'm', 'y', 'k')
     for i in range(len(y_varnames)):
-        pylab.plot(x_array, y_arrays[i], '-', label=y_varnames[i], lw=2, c=colors[i % len(colors)], alpha=0.6)
+        pylab.plot(x_array, y_arrays[i], '-', label=y_varnames[i], lw=2,
+            c=colors[i % len(colors)], alpha=0.6)
     pylab.tick_params(axis='both', which='major', labelsize=12)
     pylab.xlabel(x_label, fontsize=12)
     pylab.ylabel(y_label, fontsize=12)
@@ -69,4 +91,10 @@ if __name__ == "__main__":
         return cu(val, 'inch**2', 'm**2')
     def invert(val):
         return -val
-    plot(p, np.arange(1.0, 20.0, 0.5), 'compression_system.comp1.PR_design', ('compression_system.nozzle.Fg',), 'Pressure Ratio at MN=0.3', 'Gross thrust (lbf)', 'UW Pod Rev: 1 Nov 2015', postprocess_funcs=(invert,))
+    plot(p, x_array=np.arange(1.0, 20.0, 0.5),
+        x_varname='compression_system.comp1.PR_design',
+        y_varnames=('compression_system.nozzle.Fg',),
+        x_label='Pressure Ratio at MN=0.3',
+        y_label='Gross thrust (lbf)',
+        title='UW Pod Rev: 1 Nov 2015',
+        postprocess_funcs=(invert,))
