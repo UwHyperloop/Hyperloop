96c2b9f443e089672a8ba1ce082a2227f2b83b45
Brent Schroeter <brentsch@uw.edu>
2015-11-11 16:11:28 -0800

    Switch from pylab to pyplot

--- src/hyperloop/chart_generator.py
+++ src/hyperloop/chart_generator.py
@@ -1,13 +1,14 @@
 from hyperloop_sim import HyperloopSim
 from openmdao.units.units import convert_units as cu
-import pylab
+from matplotlib import pyplot, rcParams
 import numpy as np
 from time import time
 import sys
 from os import devnull
 
 def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
-        title='HyperloopSim', postprocess_funcs=tuple()):
+        title='HyperloopSim', postprocess_funcs=tuple(),
+        show=True, filename=''):
     '''
     Runs an OpenMDAO problem for multiple values of x and plots the specified
     results.
@@ -29,6 +30,10 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
         For each y variable, if the corresponding function in the tuple exists
         and is not equal to None, the function will be called with the y value
         as the only parameter. The returned value will be stored and plotted.
+    show : bool
+        Whether or not to open plot in a window.
+    filename : str
+        If specified, the location to save a PNG of the plot.
     '''
     progress_width = 50
     y_arrays = list([] for varname in y_varnames)
@@ -61,15 +66,26 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
     print ''
     print ''
     colors = ('r', 'g', 'b', 'c', 'm', 'y', 'k')
+
+    #rcParams['interactive'] = True
+    #rcParams['font.family'] = 'Lato'
+    #rcParams['font.weight'] = 700
+    f = pyplot.figure()
+    ax = f.add_subplot(1, 1, 1, frame_on=True, xlabel=x_label,
+            ylabel=y_label, title=title)
+    ax.tick_params(axis='both', which='major', labelsize=12)
+    f.set_size_inches(8, 5)
+
     for i in range(len(y_varnames)):
-        pylab.plot(x_array, y_arrays[i], '-', label=y_varnames[i], lw=2,
-            c=colors[i % len(colors)], alpha=0.6)
-    pylab.tick_params(axis='both', which='major', labelsize=12)
-    pylab.xlabel(x_label, fontsize=12)
-    pylab.ylabel(y_label, fontsize=12)
-    pylab.title(title, fontsize=14)
-    pylab.legend(loc='best')
-    pylab.show()
+        label = y_varnames[i] if (len(postprocess_funcs) < i or
+                postprocess_funcs[i] == None) else '* ' + y_varnames[i]
+        ax.plot(x_array, y_arrays[i], '-', label=label,
+                lw=2, c=colors[i % len(colors)], alpha=0.6)
+    ax.legend(loc='best')
+    if filename != '':
+        f.savefig(filename, dpi=130)
+    if show:
+        f.show()
 
 class PostProcess():
     @staticmethod
@@ -95,15 +111,16 @@ class PostProcess():
         return -val
 
 if __name__ == "__main__":
-    p = HyperloopSim.p_factory()
-    p['compression_system.comp1.PR_design'] = 1.0
-    p['comp1_exit_MN'] = 0.9
-    #p['percent_to_bypass'] = 0.01
-    #p['pod_MN'] = 0.3
-    plot(p, x_array=np.arange(0.1, 0.6, 0.25),
+    p = HyperloopSim.p_factory(inlet_area=0.4735, cross_section=0.8538)
+
+    # Mass flow
+    plot(p, x_array=np.arange(0.025, 0.6, 0.025),
         x_varname='pod_MN',
         y_varnames=('tube_flow.W', 'bypass_W', 'compression_system.inlet.Fl_I:stat:W'),
         x_label='Travel Mach',
         y_label='Mass flow (kg/s)',
-        title='OpenMDAO: UW Pod, Rev: 1 Nov 2015',
-        postprocess_funcs=(None, None, PostProcess.lbm2kg))
+        title='OpenMDAO: UW Pod, Rev: 10 Nov 2015',
+        postprocess_funcs=(None, None, PostProcess.lbm2kg),
+        show=True, filename='/Users/brent/Desktop/PyCycle/auto/Mass_Flow.png')
+    #p['percent_to_bypass'] = 0.01
+    #p['pod_MN'] = 0.3
