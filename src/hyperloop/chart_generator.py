46a1b86fd6f1a1655a40c31614ce14c25f5fcd8e
Brent Schroeter <brentsch@uw.edu>
2015-11-15 03:17:04 -0800

    Streamline and improve functionality

--- src/hyperloop/chart_generator.py
+++ src/hyperloop/chart_generator.py
@@ -8,7 +8,7 @@ from os import devnull
 
 def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
         title='HyperloopSim', postprocess_funcs=tuple(),
-        show=True, filename=''):
+        show=True, filename='', suppress_errs=True):
     '''
     Runs an OpenMDAO problem for multiple values of x and plots the specified
     results.
@@ -34,6 +34,9 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
         Whether or not to open plot in a window.
     filename : str
         If specified, the location to save a PNG of the plot.
+    suppress_errs : bool
+        Attempts to plot data instead of raising an exception if OpenMDAO
+        encounters an error.
     '''
     progress_width = 50
     y_arrays = list([] for varname in y_varnames)
@@ -51,14 +54,20 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
         sys.stdout.flush()
         p[x_varname] = val
         sys.stdout = open(devnull, 'w')
-        p.run()
-        sys.stdout.close()
-        sys.stdout = sys.__stdout__
-        for i in range(len(y_varnames)):
-            out = p[y_varnames[i]]
-            if len(postprocess_funcs) > i and postprocess_funcs[i] != None:
-                out = postprocess_funcs[i](out)
-            y_arrays[i].append(out)
+        try:
+            p.run()
+            sys.stdout.close()
+            sys.stdout = sys.__stdout__
+            for i in range(len(y_varnames)):
+                out = p[y_varnames[i]]
+                if len(postprocess_funcs) > i and postprocess_funcs[i] != None:
+                    out = postprocess_funcs[i](out)
+                y_arrays[i].append(out)
+        except:
+            sys.stdout.close()
+            sys.stdout = sys.__stdout__
+            print 'WARNING: Error encountered running system. Plotting prematurely.'
+            break
         opt_num += 1
     sys.stdout.write('[%s] %.2f minutes elapsed    \r' %
         ('#' * progress_width, float(elapsed_s) / 60))
@@ -67,9 +76,6 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
     print ''
     colors = ('r', 'g', 'b', 'c', 'm', 'y', 'k')
 
-    #rcParams['interactive'] = True
-    #rcParams['font.family'] = 'Lato'
-    #rcParams['font.weight'] = 700
     f = pyplot.figure()
     ax = f.add_subplot(1, 1, 1, frame_on=True, xlabel=x_label,
             ylabel=y_label, title=title)
@@ -77,7 +83,7 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
     f.set_size_inches(8, 5)
 
     for i in range(len(y_varnames)):
-        label = y_varnames[i] if (len(postprocess_funcs) < i or
+        label = y_varnames[i] if (len(postprocess_funcs) <= i or
                 postprocess_funcs[i] == None) else '* ' + y_varnames[i]
         ax.plot(x_array, y_arrays[i], '-', label=label,
                 lw=2, c=colors[i % len(colors)], alpha=0.6)
@@ -89,23 +95,10 @@ def plot(p, x_array, x_varname, y_varnames, x_label, y_label,
 
 class PostProcess():
     @staticmethod
-    def lbm2kg(val):
-        return cu(val, 'lbm', 'kg')
-    @staticmethod
-    def degR2degC(val):
-        return cu(val, 'degR', 'degC')
-    @staticmethod
-    def psi2Pa(val):
-        return cu(val, 'psi', 'Pa')
-    @staticmethod
-    def ft2m(val):
-        return cu(val, 'ft', 'm')
-    @staticmethod
-    def ft_s2mph(val):
-        return cu(val, 'ft/s', 'mi/h')
-    @staticmethod
-    def sq_in2sq_m(val):
-        return cu(val, 'inch**2', 'm**2')
+    def converter(unit1, unit2):
+        def convert(val):
+            return cu(val, unit1, unit2)
+        return convert
     @staticmethod
     def invert(val):
         return -val
@@ -122,5 +115,28 @@ if __name__ == "__main__":
         title='OpenMDAO: UW Pod, Rev: 10 Nov 2015',
         postprocess_funcs=(None, None, PostProcess.lbm2kg),
         show=True, filename='/Users/brent/Desktop/PyCycle/auto/Mass_Flow.png')
-    #p['percent_to_bypass'] = 0.01
-    #p['pod_MN'] = 0.3
+
+   # Compressor CFM
+    p['percent_into_bypass'] = 1.0 - p['inlet_area'] / p['tube_area']
+    p['compression_system.comp1.eff_design'] = 0.8
+    p['compression_system.comp1.PR_design'] = 1.5
+    plot(p, x_array=np.arange(0.05, 0.5875, 0.0125),
+            x_varname='pod_MN',
+            y_varnames=('comp1_cfm',),
+            x_label='Travel Mach',
+            y_label='CFM Entering Fan',
+            title='OpenMDAO: UW Pod, Rev: 10 Nov 2015',
+            show=True, filename='/Users/brent/Desktop/PyCycle/auto/CFM.png')
+
+    # Flow by cross section
+    p['pod_MN'] = 0.35
+    p['percent_into_bypass'] = 1.0
+    p['inlet_area'] = 0.4735
+    plot(p, x_array=np.arange(0.8538, 1.5, 0.025),
+            x_varname='cross_section',
+            y_varnames=('tube_flow.W', 'bypass_W', 'compression_system.inlet.Fl_I:stat:W'),
+            x_label='Cross Section (m^2) at Mach 0.35',
+            y_label='Mass flow (kg/s)',
+            title='OpenMDAO: UW Pod, Rev: 10 Nov 2015',
+            postprocess_funcs=(None, None, PostProcess.converter('lbm/s', 'kg/s')),
+            show=True, filename='/Users/brent/Desktop/PyCycle/auto/Cross_Section_Flow.png')
